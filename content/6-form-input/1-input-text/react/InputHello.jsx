98564bfce1e0e8f5188618a1cc384b9f1a3044fe
Arkadiy Haidarzhy <arkadiy@byzantium.com.ua>
2023-01-15 14:35:42 +0200

    Inline handling input
    In case of simple state update I think handling event inline is acceptable and make component look shorter (almost like Vue version).

--- content/6-form-input/1-input-text/react/InputHello.jsx
+++ content/6-form-input/1-input-text/react/InputHello.jsx
@@ -3,14 +3,10 @@ import { useState } from "react";
 export default function InputHello() {
   const [text, setText] = useState("Hello world");
 
-  function handleChange(event) {
-    setText(event.target.value);
-  }
-
   return (
     <>
       <p>{text}</p>
-      <input value={text} onChange={handleChange} />
+      <input value={text} onChange={(event) => {setText(event.target.value)}} />
     </>
   );
 }
